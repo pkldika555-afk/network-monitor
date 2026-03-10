1ef042264e8a3f1de8b7d56763dccbfe6d4dc29b
dikapkl <dikapkl555@example.com>
2026-03-10 09:14:26 +0700

    feat(services): make index function  and routing

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class ServiceController extends Controller
 {
-    //
+    public function index()
+    {
+        $services = Service::latest()->get();
+        $categories = Service::distinct()->pluck('category')->sort()->value();
+
+        return view ('services.index', compact('categories', 'services'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ServiceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/services', [ServiceController::class, 'index'])->name('services.index');
