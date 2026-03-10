8a1d27c910a0cbdb44139d85fcda943aa979011a
dikapkl <dikapkl555@example.com>
2026-03-10 11:02:42 +0700

    feat(services): per one second auto

--- resources/views/services/partials/_aside.blade.php
+++ resources/views/services/partials/_aside.blade.php
@@ -62,6 +62,7 @@
         </div>
         <select id="interval-sel" onchange="setIntervalTime()"
             class="w-full bg-gray-900 border border-gray-700 rounded text-xs mono text-gray-300 px-2 py-1 outline-none">
+            <option value="1">1 detik</option>
             <option value="30">30 detik</option>
             <option value="60" selected>1 menit</option>
             <option value="300">5 menit</option>
